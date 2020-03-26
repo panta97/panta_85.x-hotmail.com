01a21af5874cc70c93b5395451feacaa27a79295
Roosevelt <panta_85.x@hotmail.com>
2020-03-25 20:48:39 -0500

    Added module product_price2

---
+++ product_price2/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product price 2",
+
+    'summary': """
+        Agrega un nuevo compo para precio 2 en el form del producto
+        """,
+
+    'description': """
+        Agrega un nuevo compo para precio 2 en el form del producto
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    'category': 'Sale',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['product'],
+
+    # always loaded
+    'data': [
+        "views/product_view.xml"
+    ]
+}
